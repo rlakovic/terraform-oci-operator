14794649fa9c6185b146b40ad867393b698dfcb3
rlakovic <lakovicradivoje84@gmail.com>
2023-07-13 23:16:55 +0200

    Update instance_principal.tf

--- instance_principal.tf
+++ instance_principal.tf
@@ -1,35 +1,35 @@
 # Copyright (c) 2022 Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-#resource "random_id" "dynamic_group_suffix" {
-#  keepers = {
-#    # Generate a new suffix only when variables are changed
-#    label_prefix = local.dynamic_group_prefix
-#    tenancy_id   = var.tenancy_id
-#  }
+resource "random_id" "dynamic_group_suffix" {
+  keepers = {
+    # Generate a new suffix only when variables are changed
+    label_prefix = local.dynamic_group_prefix
+    tenancy_id   = var.tenancy_id
+  }
 
-#  byte_length = 8
-#}
+  byte_length = 8
+}
 
-#resource "oci_identity_dynamic_group" "operator_group" {
-#  provider = oci.home
+resource "oci_identity_dynamic_group" "operator_group" {
+  provider = oci.home
 
-#  compartment_id = random_id.dynamic_group_suffix.keepers.tenancy_id
-#  description    = "dynamic group %{ if var.label_prefix != "none" }with label ${var.label_prefix}%{ endif } to allow operator to invoke services"
+  compartment_id = random_id.dynamic_group_suffix.keepers.tenancy_id
+  description    = "dynamic group %{ if var.label_prefix != "none" }with label ${var.label_prefix}%{ endif } to allow operator to invoke services"
 
-#  lifecycle {
-#    ignore_changes = [defined_tags, name]
-#  }
+  lifecycle {
+    ignore_changes = [defined_tags, name]
+  }
 
-#  matching_rule = "ALL {instance.id = '${join(",", data.oci_core_instance.operator.*.id)}'}"
-#  name           = join("-", compact([
-#    random_id.dynamic_group_suffix.keepers.label_prefix,
-#    "operator-instance-principal",
-#    random_id.dynamic_group_suffix.hex
-#  ]))
+  matching_rule = "ALL {instance.id = '${join(",", data.oci_core_instance.operator.*.id)}'}"
+  name           = join("-", compact([
+    random_id.dynamic_group_suffix.keepers.label_prefix,
+    "operator-instance-principal",
+    random_id.dynamic_group_suffix.hex
+  ]))
 
-#  count = var.enable_operator_instance_principal == true ? 1 : 0
-#}
+  count = var.enable_operator_instance_principal == true ? 1 : 0
+}
 
 #resource "oci_identity_policy" "operator_group_policy" {
 #  provider = oci.home
